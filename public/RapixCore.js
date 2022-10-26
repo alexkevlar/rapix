e12565e10980d5b6fdef29926b03f1094632feaf
Alessandro Pano <alessandropano@gmail.com>
2022-10-26 17:30:57 +0200

    v0.7.11-beta

--- public/RapixCore.js
+++ public/RapixCore.js
@@ -21,12 +21,17 @@ const fn = {
     randomIntFromInterval(min, max) {
         if (!max)
             max = min;
-        const randomBuffer = new Uint32Array(1);
-        window.crypto.getRandomValues(randomBuffer);
-        let randomNumber = randomBuffer[0] / (0xffffffff + 1);
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(randomNumber * (max - min + 1)) + min;
+        if (crypto && "getRandomValues" in crypto) {
+            const randomBuffer = new Uint32Array(1);
+            crypto.getRandomValues(randomBuffer);
+            let randomNumber = randomBuffer[0] / (0xffffffff + 1);
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(randomNumber * (max - min + 1)) + min;
+        }
+        else {
+            return Math.floor(Math.random() * (max - min + 1) + min);
+        }
     }
 };
 const mockFailDefaults = {
--- src/public/RapixCore.ts
+++ src/public/RapixCore.ts
@@ -83,16 +83,23 @@ export interface APIOptions {
 const fn = {
   
   randomIntFromInterval(min: number, max?: number) { // min and max included
+
     if (!max) max = min;
-    const randomBuffer = new Uint32Array(1);
-    
-    window.crypto.getRandomValues(randomBuffer);
-    
-    let randomNumber = randomBuffer[0] / (0xffffffff + 1);
-    
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(randomNumber * (max - min + 1)) + min;
+
+    if (crypto && "getRandomValues" in crypto) {
+      const randomBuffer = new Uint32Array(1);
+
+      crypto.getRandomValues(randomBuffer);
+
+      let randomNumber = randomBuffer[0] / (0xffffffff + 1);
+
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(randomNumber * (max - min + 1)) + min;
+    } else {
+      return Math.floor(Math.random() * (max - min + 1) + min)
+    }
+
   }
 
 }
